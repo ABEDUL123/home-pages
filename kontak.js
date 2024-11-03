410e374818d677eb051b46cf8698f8bfd54158d2
ABEDUL123 <abdulrahmannasik333@gmail.com>
2024-11-03 10:29:25 +0700

    Update kontak.js

--- kontak.js
+++ kontak.js
@@ -1,15 +1,20 @@
 function sendMail() {
+  // Mengambil nilai dari input
   var params = {
     name: document.getElementById("name").value,
     email: document.getElementById("email").value,
     message: document.getElementById("message").value,
   };
 
+  // Mengirim email menggunakan EmailJS
   emailjs
     .send("service_81aqzr8", "template_pggug05", params)
     .then((res) => {
       console.log(res);
-      alert("Your Message Send Successfull!");
+      alert("Your message has been sent successfully!");
     })
-    .catch((err) => console.log(err));
+    .catch((err) => {
+      console.error("Error:", err);
+      alert("There was an error sending your message. Please try again.");
+    });
 }
